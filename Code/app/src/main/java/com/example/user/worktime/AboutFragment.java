b0015b724d0915950628fb4a32660f04d1672ccc
HelNi <h.killed@gmail.com>
2017-07-07 10:34:06 +0200

    create content for about-page

--- Code/app/src/main/java/com/example/user/worktime/AboutFragment.java
+++ Code/app/src/main/java/com/example/user/worktime/AboutFragment.java
@@ -3,9 +3,12 @@ package com.example.user.worktime;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
 
 /**
  * Created by User on 06.07.2017.
@@ -20,6 +23,16 @@ public class AboutFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.about_fragment, container, false);
+        View inflate = inflater.inflate(R.layout.about_fragment, container, false);
+
+        //Make the link to GitHub clickable.
+        TextView gitHub_TextView = (TextView) inflate.findViewById(R.id.github_link);
+        gitHub_TextView.setMovementMethod(LinkMovementMethod.getInstance());
+
+        //Make the link to the imprint-page clickable.
+        TextView imprint_TextView = (TextView) inflate.findViewById(R.id.imprint);
+        imprint_TextView.setMovementMethod(LinkMovementMethod.getInstance());
+
+        return inflate;
     }
 }
