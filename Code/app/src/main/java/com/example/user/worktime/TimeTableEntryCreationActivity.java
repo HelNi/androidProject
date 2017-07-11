cc704c5abf2d6b76d80b086e098ab2e7dfa52e16
Bluegrammer <phil.hansen1@hotmail.com>
2017-07-11 14:21:36 +0200

    Implement spinner's behaviour in TimeTableEntryCreationActivity

--- Code/app/src/main/java/com/example/user/worktime/TimeTableEntryCreationActivity.java
+++ Code/app/src/main/java/com/example/user/worktime/TimeTableEntryCreationActivity.java
@@ -28,13 +28,16 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.EnumSet;
 import java.util.List;
 
 import com.example.user.worktime.R;
@@ -48,6 +51,8 @@ public class TimeTableEntryCreationActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time_table_entry_creation);
+
+        addItemsOnSpinner();
     }
 
     public void startTimePicker(View view) {
@@ -55,12 +60,29 @@ public class TimeTableEntryCreationActivity extends Activity {
 
         Bundle args = new Bundle();
         args.putInt("viewId", view.getId());
-        fragment.setArguments(args);
 
+        fragment.setArguments(args);
 
         fragment.setEnterTransition(new Fade());
         fragment.setExitTransition(new Fade());
         fragment.show(getFragmentManager(),"TimePicker");
     }
+
+    // Add all values of the Activity enum into the spinner
+    private void addItemsOnSpinner() {
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        List<String> list = new ArrayList<String>();
+        EnumSet<com.example.user.worktime.Classes.TimeTable.Activity> activities = EnumSet.allOf(com.example.user.worktime.Classes.TimeTable.Activity.class);
+
+        for (com.example.user.worktime.Classes.TimeTable.Activity act : activities) {
+            list.add(act.toString());
+        }
+        
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(dataAdapter);
+    }
+
 }
 
