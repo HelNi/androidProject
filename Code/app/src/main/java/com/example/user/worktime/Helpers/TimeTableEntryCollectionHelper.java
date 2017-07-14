bbfe481bcaa4fcfe421871d6d11f0bae1be4f582
nSchick <nschicker@icloud.com>
2017-07-14 12:36:11 +0200

    Test a calculation method for gaps in the time table.

---
+++ Code/app/src/main/java/com/example/user/worktime/Helpers/TimeTableEntryCollectionHelper.java
@@ -0,0 +1,45 @@
+package com.example.user.worktime.Helpers;
+
+import android.util.SparseArray;
+
+import com.example.user.worktime.Classes.TimeTable.TimeTableEntry;
+
+import org.joda.time.Duration;
+import org.joda.time.LocalDateTime;
+import org.joda.time.Minutes;
+
+import java.util.List;
+
+/**
+ * Created by User on 14.07.2017.
+ */
+
+public class TimeTableEntryCollectionHelper {
+
+    /**
+     * @return Hashmap, the keys are the position (0 is right after the first element), the second the
+     * duration of the gap.
+     */
+    public SparseArray<Duration> gapLengths(List<TimeTableEntry> entries) {
+        LocalDateTime lastEnd = null;
+        int i = 0;
+        SparseArray<Duration> returnValue = new SparseArray<>();
+
+        for (TimeTableEntry entry : entries) {
+            if (lastEnd == null) {
+                lastEnd = entry.getEnd();
+                ++i;
+                continue; // Skip the first entry of the day.
+            }
+
+
+            Duration duration = new Duration(entry.getStart().toDateTime(), lastEnd.toDateTime());
+            if (duration.getStandardMinutes() > 0) {
+                returnValue.append(i, duration);
+            }
+            ++i;
+            lastEnd = entry.getEnd();
+        }
+        return returnValue;
+    }
+}
